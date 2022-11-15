548bfe0097c13c9909a9e63e062ba84d1c61ab40
chenxiaoming <chenxiaoming@360.cn>
2022-11-15 14:49:37 +0800

    修改打包目录

--- vue.config.js
+++ vue.config.js
@@ -24,8 +24,8 @@ module.exports = {
    * In most cases please use '/' !!!
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
-  publicPath: '/',
-  outputDir: 'docs',// dist
+  publicPath: '.', // /
+  outputDir: 'dist',// dist
   assetsDir: 'static',
   lintOnSave: process.env.NODE_ENV === 'development',
   productionSourceMap: false,
