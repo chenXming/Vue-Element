13923fef852e96fc20ceaf10648e730598f15452
chenxiaoming <chenxiaoming@360.cn>
2022-11-15 17:52:38 +0800

    提交修改

--- vue.config.js
+++ vue.config.js
@@ -24,7 +24,7 @@ module.exports = {
    * In most cases please use '/' !!!
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
-  publicPath: '', // 修改这里的路径为 . 是当前目录 为了自动发布到github，原来为 /,改为相对路径，可以部署在任意路径下
+  publicPath: '.', // 修改这里的路径为 . 是当前目录 为了自动发布到github，原来为 /,改为相对路径，可以部署在任意路径下
   outputDir: 'dist', // dist
   assetsDir: 'static',
   lintOnSave: process.env.NODE_ENV === 'development',
